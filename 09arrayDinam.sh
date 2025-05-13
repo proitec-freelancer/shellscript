7f47f1eee998e00054cda95276239e9f699e6fdb
proitec.luisinfraweb <proitec.luisinfraweb@gmail.com>
2025-05-13 16:11:29 -0400

    FIX: Se modifica scripts 09

--- 09arrayDinam.sh
+++ 09arrayDinam.sh
@@ -8,3 +8,16 @@ marcas[2]="mosarella"
 echo "Todos las marcas del arreglo: ${marcas[@]}" # Mostramos todos los elementos del arreglo
 echo "Los subindices de los elementos son: ${!marcas[@]}"
 echo "La cantidad de elementos es: ${#marcas[@]}"
+
+marcas[3]="La tranquera" # primera forma Agregamos un nuevo elemento, recordar que los scripts van en cascada
+marcas+=("Runasss") # Segunda forma Agregamos un nuevo elemento, recordar que los scripts van en cascada
+
+echo "Todos las marcas del arreglo: ${marcas[@]}" # Mostramos todos los elementos del arreglo
+echo "Los subindices de los elementos son: ${!marcas[@]}"
+echo "La cantidad de elementos es: ${#marcas[@]}"
+
+marcas[2]="Pipore"
+
+echo "Todos las marcas del arreglo: ${marcas[@]}" # Mostramos todos los elementos del arreglo
+echo "Los subindices de los elementos son: ${!marcas[@]}"
+echo "La cantidad de elementos es: ${#marcas[@]}"
